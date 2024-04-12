c7676ae2cc2b951927312bbc0531fa49080dd966
JoaoBringmann <149213160+JoaoBringmann@users.noreply.github.com>
2024-04-12 11:55:04 -0300

    Final 2

--- Exer10.py
+++ Exer10.py
@@ -1,10 +1,16 @@
 lista = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6]
 counts = {}
+frequencia = []
+
 for num in lista:
     if num in counts:
         counts[num] += 1
     else:
         counts[num] = 1
+
 max_count = max(counts.values())
-frequencia = [num for num, count in counts.items() if count == max_count]
+for num, count in counts.items():
+    if count == max_count:
+        frequencia.append(num)
+
 print(frequencia)
